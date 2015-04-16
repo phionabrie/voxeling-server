73e3a0a16a25bc595dccb6c0d6c9748682eda8cc
Alan Szlosek <szlosek@deviantart.com>
2015-04-16 08:58:44 -0400

    getting closer on terrain generation

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Very much in flux, at the moment.
 `npm install` should satisfy dependencies. However, you'll still need to checkout the latest voxel-server and modify index.js to point to it.
 
 Then run the server with `node index.js`
+
+My ultimate goal:
+
+* Have a game server I can self-host
+* With a persistent world (backed by mysql, likely)
+* So I can invite friends and members of my meetup to play around
--- index.js
+++ index.js
@@ -1,31 +1,77 @@
 // Requires the latest checkout of https://github.com/kumavis/voxel-server. Not sure how it differs from npm version
 var Server = require('/Users/alanszlosek/Projects/voxel-server')
 var perlin = require('voxel-perlin-terrain')
-var generator = perlin(Date.now(), 0, 5, 20)
+var generator = perlin('moo', 0, 5, 20)
 var websocket = require('websocket-stream')
 var voxel = require('voxel')
-
+var ndarray = require('ndarray')
+var chunkSize = 32
+var voxelsByChunk = {
+}
+//var generator = voxel.generator['Hilly Terrain']
 
 var settings = {
   // various [voxel-engine]() settings to be sent to the clients
-  avatarInitialPosition: [2, 20, 2],
+  avatarInitialPosition: [0, 60, 0],
   // list of incomming custom events to forward to all clients
   //forwardEvents: ['attack','voiceChat']
 
-  //generateChunks: false
-  //forwardEvents: { }
-  generate: voxel.generator['Hill']
+  // custom
+  //generateChunks: false,
+  //generate: voxel.generator['Hill'],
+  controls: {
+    discreteFire: false,
+    //speed: Number(1),
+    //maxWalkSpeed: Number(10),
+    //jumpSpeed: Number(.001),
+    jumpMaxSpeed: Number(.010)
+  },
+  generate: function(x,y,z) {
+    var xx = Math.abs(x % chunkSize)
+    var yy = Math.abs(y % chunkSize)
+    var zz = Math.abs(z % chunkSize)
+    // which chunk are we at?
+    var chunkPosition = '' + Math.floor(x/chunkSize)
+        + '|'
+        + Math.floor(y/chunkSize)
+        + '|'
+        + Math.floor(z/chunkSize)
+    var data
+    if (!(chunkPosition in voxelsByChunk)) {
+        console.log('Generating chunk ' + chunkPosition)
+        voxelsByChunk[chunkPosition] = generator([xx, yy, zz], chunkSize)
+    }
+    data = voxelsByChunk[chunkPosition]
+    return data[xx + yy*chunkSize + zz*chunkSize*chunkSize]
+  }
 }
 
 // create server
 var server = Server(settings)
 
+// Generate our first chunk
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+/* 
+  game.voxels.on('missingChunk', function(p) {
+    var chunkSize = 32;
+    var voxels = generator(p, chunkSize)
+    var chunk = {
+      position: p,
+      dims: [chunkSize, chunkSize, chunkSize],
+      voxels: voxels
+    }
+    game.showChunk(chunk)
+  })
+*/
 
 // These don't seem to work
 // bind events
 server.on('missingChunk', function(chunk){
-console.log('missing chunk');
-    console.log(chunk);
+  console.log('missing chunk');
 return;
     var chunkSize = 32;
     chunk.voxels = generator(chunk.position, chunkSize)
