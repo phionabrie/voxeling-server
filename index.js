c81728b7218e8214bc818e32d93ebef03577eadf
Alan Szlosek <szlosek@deviantart.com>
2015-04-26 12:30:16 -0400

    use non-repo voxel-server module

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 // Requires the latest checkout of https://github.com/kumavis/voxel-server. Not sure how it differs from npm version
-var Server = require('/Users/alanszlosek/Projects/voxel-server')
-//var generator = require('voxel-perlin-terrain')('moo', 0, 5)
+var Server = require('voxel-server')
+var chunkSize = 32
+/*
 var generator = require('voxel-simplex-terrain')({
-    chunkDistance: 2,
-    chunkSize: 32,
-    seed: 98237498723987234
+    chunkDistance: 3,
+    scaleFactor: 10,
+    seed: 0.4658187120221555
 })
+*/
 var websocket = require('websocket-stream')
-var voxel = require('voxel')
 
 var settings = {
+  chunkSize: chunkSize,
+  chunkDistance: 4,
   // various [voxel-engine]() settings to be sent to the clients
   avatarInitialPosition: [3, 60, 3],
+  worldOrigin: [0,0,0],
   // list of incomming custom events to forward to all clients
   //forwardEvents: ['attack','voiceChat']
 
   // custom
-  //generateChunks: false,
-  //generate: voxel.generator['Hill'],
   controls: {
     discreteFire: false,
     //speed: Number(1),
@@ -25,7 +27,31 @@ var settings = {
     //jumpSpeed: Number(.001),
     jumpMaxSpeed: Number(.010)
   },
+
+/*
+  avatarInitialPosition: [50,100,50],
+  materials: ['grass', 'obsidian', 'dirt', 'whitewool', 'crate', 'brick', 'brick'],
   generateVoxelChunk: generator
+*/
+
+  generateChunks: true,
+  // 1 block rise, each ring of chunks out from center
+  generate: function(x, y, z) {
+    var chunkX = Math.abs(Math.floor(x / chunkSize))
+    var chunkY = Math.abs(Math.floor(y / chunkSize))
+    var chunkZ = Math.abs(Math.floor(z / chunkSize))
+
+    if (y == 0) {
+      return 1
+    }
+
+    var out = Math.max(chunkX, chunkZ)
+    if (y > 0 && y <= out) {
+        return 1
+    }
+    return 0
+  }
+
 }
 
 // create server
